aeba8d9f0677bdee145f78bf7ff448cd784cabb4
FRANCO SCARPELLO <FScarpello@prismamp.com>
2019-09-28 16:38:43 -0300

    codigo incial arduino

--- Sistema-Embebido/code/sketch/sketch.ino
+++ Sistema-Embebido/code/sketch/sketch.ino
@@ -2,97 +2,82 @@
  * SerialOutput sketch
  * Print numbers to the serial port
 */
+#include "lib/Kalman.h"
+
+unsigned long interval = 4000; // the time we need to wait
+unsigned long previousMillis = 0; // millis() returns an unsigned long.
 
 const int flexSensorMedio = A0;
+const int flexSensorIndice = A1;
+const int flexSensorPulgar = A2;
 
 // Measure the voltage at 5V and the actual resistance of your
 // 47k resistor, and enter them below:
-const float VCC = 4.98; // Measured voltage of Ardunio 5V line
-const float R_DIV = 98.0; // Measured resistance of 3.3k resistor
+#define VCC = 4.98 // Measured voltage of Ardunio 5V line
 
 // Upload the code, then try to adjust these values to more
 // accurately calculate bend degree.
-const float STRAIGHT_RESISTANCE = 550.0; // resistance when straight
-const float BEND_RESISTANCE = 75.0; // resistance at 90 deg
+#define STRAIGHT_RESISTANCE_FLEX_MEDIO  550.0 // resistance when straight
+#define BEND_RESISTANCE_FLEX_MEDIO      75.0 // resistance at 90 deg
+#define DIVISOR_RESISTANCE_FLEX_MEDIO   98.0 // Measured resistance of 3.3k resistor
+
+// falta sacar medicion de este
+#define STRAIGHT_RESISTANCE_FLEX_INDICE  550.0 // resistance when straight
+#define BEND_RESISTANCE_FLEX_INDICE      75.0 // resistance at 90 deg
+#define DIVISOR_RESISTANCE_FLEX_INDICE   98.0 // Measured resistance of 3.3k resistor
+
+// falta sacar medicion de este
+#define STRAIGHT_RESISTANCE_FLEX_PULGAR  550.0 // resistance when straight
+#define BEND_RESISTANCE_FLEX_PULGAR      75.0 // resistance at 90 deg
+#define DIVISOR_RESISTANCE_FLEX_PULGAR   98.0 // Measured resistance of 3.3k resistor
+
+//Creates the variables:
+double measurement, filteredMeasurement;
+Kalman myFilter(0.125,32,1023,0); //suggested initial values for high noise filtering
 
 void setup()
 {
   pinMode(flexSensorMedio, INPUT);
+  pinMode(flexSensorIndice, INPUT);
+  pinMode(flexSensorPulgar, INPUT);
   Serial.begin(9600); // Initialize Serial communciation - send and receive at 9600 baud
 }
 
 
-void loop()
+void loop() 
 {
-  // Read the ADC, and calculate voltage and resistance from it
-  int flexADC = analogRead(flexSensorMedio);
-  unsigned int kalmanValue;
-  float flexV = flexADC * VCC / 1023.0;
-  float flexR = R_DIV * (VCC / flexV - 1.0);
-  Serial.println("Resistance: " + String(flexR) + " ohms");
+  unsigned long currentMillis = millis(); // grab current time
+ 
+  // Si interval es menor a la diferencia de currentMillis y previousMillis tomo las mediciones de los sensores.
+  if ((unsigned long)(currentMillis - previousMillis) >= interval) {
+    float algo = readFlex(flexSensorMedio, STRAIGHT_RESISTANCE_FLEX_MEDIO, BEND_RESISTANCE_FLEX_MEDIO, DIVISOR_RESISTANCE_FLEX_MEDIO);
+    float algo2 = readFlex(flexSensorPulgar, STRAIGHT_RESISTANCE_FLEX_PULGAR, BEND_RESISTANCE_FLEX_PULGAR, DIVISOR_RESISTANCE_FLEX_PULGAR);
+    float algo3 = readFlex(flexSensorIndice, STRAIGHT_RESISTANCE_FLEX_INDICE, BEND_RESISTANCE_FLEX_INDICE, DIVISOR_RESISTANCE_FLEX_INDICE);
 
-  // Use the calculated resistance to estimate the sensor's
-  // bend angle:
-  float angle = map(flexR, STRAIGHT_RESISTANCE, BEND_RESISTANCE,
-    0, 90.0);
-  Serial.println("Bend: " + String(angle) + " degrees");
+    
+    // Actualizo previousMillis para que vuelva a ingresar en próximas iteraciones.
+    previousMillis = millis();
+ }
 
-  kalmanValue = Kalman_filter_for_SEN_1(flexADC);
-  Serial.println("KalmanValue: " + String(kalmanValue) + " kalman filter");
+  //reads measurement and filter it
+
+  /*
+  // only for kalman
+  filteredMeasurement = myFilter.getFilteredValue(measurement);
+  Serial.println("KalmanFilter: " + String(filteredMeasurement) + " kalmanValue");
   Serial.println();
-
-  delay(4000);
+*/
 }
 
-
-unsigned int Kalman_filter_for_SEN_1(signed int ADC_Value)
+float readFlex (const int sensorName, const int straightResistance, const int bendResistance, const int divisorResistance)
 {
-  static unsigned char counter = 0;
-  static float A,H,Q,R,X,P;
-  float XP,PP;
-  static float value_return=0;
-  float K;
-  float temp_float;
- 
-  if(counter < 1)
-     counter++;
- 
-  if(counter == 1)
-  {
-    A = 1;
-    H = 1;   //1
-    Q = 0.22;     //0.32 earlier 0.92,0.02
-    R = 0.9;     // 0.8
-    X = 1023;
-    P = 6;
-    counter = 2;
- 
-    XP = A*X;
-    PP = A*P*A + Q;
- 
-    K = PP * H;
-    K /= (H * H * PP) + R;
- 
-    temp_float = (float)(ADC_Value - H * XP);
-    value_return = XP + K * temp_float;
- 
-    temp_float = H*PP;
-    P = PP-K*temp_float;
-    return (unsigned int)value_return;
-  }
-  else
-  {
-    XP = A*value_return;
-    PP = A*P*A + Q;
- 
-    K = PP*H;
-    K /=(H*H*PP)+R;
- 
-    temp_float = (float)(ADC_Value-H*XP);
-    value_return = XP + K*temp_float;
- 
-    temp_float = (float)H*PP;
-    P = PP-K*temp_float;
-    return (unsigned int)value_return;
-  }
+  int sensorValue = analogRead(sensorName); //read new value from sensor in analog pin A1, for example
+
+  float flexVoltaje = sensorValue * VCC / 1023.0;
+  float flexResistance = divisorResistance * (VCC / flexVoltaje - 1.0);
+
+  // Uso la resistencia calculada para estimar el angulo de inclinación del sensor
+  float angleValue = map(flexResistance, straightResistance, bendResistance, 0, 90.0);
+
+  return angleValue;
 }
