2da315db26a2d3020819c3414508d6d8e141c92f
FRANCO SCARPELLO <FScarpello@prismamp.com>
2019-11-02 17:24:14 -0300

    Logica flex, gesture, hand, parte de main, resta resolver constructor by ref

--- Sistema-Embebido/sketch/lib/Flex.h
+++ Sistema-Embebido/sketch/lib/Flex.h
@@ -3,16 +3,33 @@
 
 #define BEND_FLEX         0
 #define STRAIGHT_FLEX     1
+#define VOLTAJE_RECEIVED	4.98
 
 class Flex {
 	private:
 		double straightResistance; 	// Resistencia del flex estirado.
 		double bendResistance;		// Resistencia del flex flexionado.
 		double divisorResistance;	// Resistencia del divisor de tensión.
-		double voltaje;				// Voltaje que recibe el flex.
-		int flexPosition;				// Posición en la que se encuentra el dedo.
+		double flexVoltaje;				// Voltaje que recibe el flex.
+		int flexPosition;			// Posición en la que se encuentra el dedo.
+		double actualValue;
+		double angleV;
 	
 	public:
+		// Constructor genérico.
+		Flex() {};
+
+		// Constructor a partir de otro objeto
+		Flex(const Flex& newFlex) {
+			this->straightResistance = newFlex.straightResistance;
+			this->bendResistance = newFlex.bendResistance;
+			this->divisorResistance = newFlex.divisorResistance;
+			this->flexVoltaje = newFlex.flexVoltaje;
+			this->flexPosition = newFlex.flexPosition;
+			this->actualValue = newFlex.actualValue;
+			this->angleV = newFlex.angleV;
+		};
+
 		/*
 		* Constructor del Flex.
 		* Entrada:
@@ -24,6 +41,8 @@ class Flex {
 			this->divisorResistance = divisorResistance;
 			this->flexVoltaje = -1;
 			int flexPosition = -1;
+			this->actualValue = -1;
+			this->angleV = -1;
 		}
 		
 		/*
@@ -32,8 +51,10 @@ class Flex {
 		* 	@sensorValue: valor leído del sensor flex.
 		*	@voltajeReceived: voltaje que recibe el sensor flex.
 		*/
-		void processInformation(double sensorValue, double voltajeReceived) {
-			flexPositionCalculator (angleCalculator(sensorValue, voltajeReceived));
+		double processInformation(double sensorValue) {
+			this->actualValue = sensorValue;
+			flexPositionCalculator (angleCalculator(sensorValue));
+			return sensorValue;
 		}
 		
 		/*
@@ -68,15 +89,27 @@ class Flex {
 			return this->flexPosition;
 		}
 		
+		double getAngle() {
+			return this->angleV;
+		}
+		
+		double getActualValue() {
+			return this->actualValue;
+		}
+		
 	private:
-		double angleCalculator(double sensorValue, double voltajeReceived) {
+		double angleCalculator(double sensorValue) {
 			double flexResistance;
 			
-			this->flexVoltaje = sensorValue * voltajeReceived / 1023.0;
-			flexResistance = this->divisorResistance * (voltajeReceived / this->flexVoltaje - 1.0);
+			this->flexVoltaje = (sensorValue - this->divisorResistance) * (double)VOLTAJE_RECEIVED / 1023.0;
+			flexResistance = this->divisorResistance * ((double)VOLTAJE_RECEIVED / this->flexVoltaje - 1.0);
 
 			// Uso la resistencia calculada para estimar el angulo de inclinación del sensor
-			angle = map(flexResistance, this->straightResistance, this->bendResistance, 0, 90.0);
+			double angle = map(flexResistance, this->straightResistance, this->bendResistance, 0, 90.0);
+			
+			
+			this->angleV = angle;
+			
 			return angle;
 		}
 		
@@ -85,12 +118,12 @@ class Flex {
 				this->flexPosition = (int)BEND_FLEX;
 				return;
 			}
-			else if (flexAngle > 45.0){
+			else {
 				this->flexPosition = (int)STRAIGHT_FLEX;
 				return;
 			}
 		}
 		
-}
+};
 
 #endif
--- Sistema-Embebido/sketch/sketch.ino
+++ Sistema-Embebido/sketch/sketch.ino
@@ -2,21 +2,23 @@
    SerialOutput sketch
    Print numbers to the serial port
 */
-#include "lib/Kalman.h" // para quitar el ruido de la medición del acelerómetro.
+#include "lib/Flex.h"
+#include "lib/Hand.h"
+//#include "lib/Gesture.h"
 #include <SoftwareSerial.h>    // para la comunicación bluetooth
 
 /*
    Utilizadas para marcar el intervalo de lectura de medición de los sensores.
 */
-unsigned long interval = 1000; // the time we need to wait
+unsigned long interval = 4000; // the time we need to wait
 unsigned long previousMillis = 0; // millis() returns an unsigned long.
 
 /*
    Asignación de sensores a los distintos pines del arduino.
 */
-#define FLEX_SENSOR_PULGAR  A0
+#define FLEX_SENSOR_PULGAR  A2
 #define FLEX_SENSOR_INDICE  A1
-#define FLEX_SENSOR_MEDIO   A2
+#define FLEX_SENSOR_MEDIO   A0
 #define BLUETOOTH_RX        0
 #define BLUETOOTH_TX        1
 #define BLUE_LED            2
@@ -50,63 +52,23 @@ unsigned long previousMillis = 0; // millis() returns an unsigned long.
 #define BEND_RESISTANCE_FLEX_MEDIO      504.0 // resistance at 90 deg
 #define DIVISOR_RESISTANCE_FLEX_MEDIO   98.0 // Measured resistance of 3.3k resistor
 
-/*
- * Valor de cada movimiento.
- * Es el valor que se enviará vía Bluetooth a la aplicación Android.
- */
-#define PLAY_PAUSE          0
-#define NEXT                1
-#define PREVIOUS            2
-#define SPEED_X1            3
-#define SPEED_X2            4
-#define SPEED_X05           5
-#define NOT_VALID_GESTURE   8
-#define NOT_MOVEMENT        9
-
-/*
- * Valor de la posición de cada sensor flex.
- */
-#define BEND_FLEX         0
-#define MEDIUM_BEND_FLEX  1
-#define STRAIGHT_FLEX     2
-
-/*
- * Valor del sentido al que se mueve el acelerómetro.
- */
-#define LEFT  0
-#define RIGHT 1
-
-
-/*
-   Variables utilizadas para Kalman e instanciación de un objeto de Kalman.
-*/
-double measurement;
-double filteredMeasurement;
-Kalman myFilter(0.125, 32, 1023, 0); //suggested initial values for high noise filtering
-
-/* 
- *  Variables globales utilizzadas para controlar los movimientos de la mano.
- */
-int previousMovement = (int) NOT_MOVEMENT;
-int newMovement = (int) NOT_MOVEMENT;
-double angleFlexPulgar;
-double angleFlexIndice;
-double angleFlexMedio;
-int flexPulgarPosition;
-int flexIndicePosition;
-int flexMedioPosition;
-
 // Valor a enviar vía bluetooth.
 int mensajeBluetooth;
 
 // Declarar objeto de comunicación bluetooth.
 //SoftwareSerial bluetooth(BLUETOOTH_RX, BLUETOOTH_TX);
 
+// Declarar objetos de sensores
+Flex bigFinger((double)DIVISOR_RESISTANCE_FLEX_PULGAR);
+Flex indexFinger((double)DIVISOR_RESISTANCE_FLEX_INDICE);
+Flex middleFinger((double)DIVISOR_RESISTANCE_FLEX_MEDIO);
+Hand hand(bigFinger, indexFinger, middleFinger);
+//Gesture gesture(hand);
 
-// PROTOTIPOS DE FUNCIONES
-double readFlex (const int, const int, const int, const int);
-int convertAngleIntoPosition (const float);
-int convertInformationIntoMovement (const int, const int, const int, const int);
+// Prototipos de funciones
+void sendInformationViaBluetooth (const int);
+void rgbColor (int, int, int);
+
 
 /*
    Configuración inicial de pines y comunicación.
@@ -116,7 +78,7 @@ void setup()
   pinMode(FLEX_SENSOR_PULGAR, INPUT);
   pinMode(FLEX_SENSOR_INDICE, INPUT);
   pinMode(FLEX_SENSOR_MEDIO, INPUT);
-  pinMode(BLUE_LED, OUTPUT);
+  //pinMode(BLUE_LED, OUTPUT);
   /*pinMode(RGB_LED_RED, OUTPUT);
   pinMode(RGB_LED_BLUE, OUTPUT);
   pinMode(RGB_LED_GREEN, OUTPUT);*/
@@ -133,48 +95,65 @@ void loop()
   // Obtener tiempo actual
   unsigned long currentMillis = millis();
 
+  if (hand.getCalibrated() == false){
+      // calibro dedos
+      hand.calibrateStraightHand((double)STRAIGHT_RESISTANCE_FLEX_PULGAR, (double)STRAIGHT_RESISTANCE_FLEX_INDICE,(double)STRAIGHT_RESISTANCE_FLEX_MEDIO); 
+      hand.calibrateStraightHand((double)BEND_RESISTANCE_FLEX_PULGAR, (double)BEND_RESISTANCE_FLEX_INDICE,(double)BEND_RESISTANCE_FLEX_MEDIO); 
+      hand.setCalibrated();;
+  }
+
   // Si interval es menor a la diferencia de currentMillis y previousMillis tomo las mediciones de los sensores.
   if ((unsigned long)(currentMillis - previousMillis) >= interval)
   {
     /*if(bluetooth.available())
     {
       digitalWrite(BLUE_LED, HIGH);*/
-      angleFlexPulgar = readFlex(FLEX_SENSOR_PULGAR, STRAIGHT_RESISTANCE_FLEX_PULGAR, BEND_RESISTANCE_FLEX_PULGAR, DIVISOR_RESISTANCE_FLEX_PULGAR);
-      angleFlexIndice = readFlex(FLEX_SENSOR_INDICE, STRAIGHT_RESISTANCE_FLEX_INDICE, BEND_RESISTANCE_FLEX_INDICE, DIVISOR_RESISTANCE_FLEX_INDICE);
-      angleFlexMedio = readFlex(FLEX_SENSOR_MEDIO, STRAIGHT_RESISTANCE_FLEX_MEDIO, BEND_RESISTANCE_FLEX_MEDIO, DIVISOR_RESISTANCE_FLEX_MEDIO);
-  
-      flexPulgarPosition = convertAngleIntoPosition(angleFlexPulgar);
-      flexIndicePosition = convertAngleIntoPosition(angleFlexIndice);
-      flexMedioPosition = convertAngleIntoPosition(angleFlexMedio);
-  
-      newMovement = convertInformationIntoMovement (flexPulgarPosition, flexIndicePosition, flexMedioPosition, 5);
+      // llamar a hand
+      hand.process((double)analogRead(FLEX_SENSOR_PULGAR), (double)analogRead(FLEX_SENSOR_INDICE), (double)analogRead(FLEX_SENSOR_MEDIO));
 
-      Serial.println("movimientos:");
-      Serial.println(previousMovement);
-      Serial.println(newMovement);
-      // si el movimiento es distinto al anterior envío la información a la aplicación Android.
-      if (newMovement != previousMovement && newMovement != (int) NOT_VALID_GESTURE)
-      {
-        Serial.println("El movimiento es nuevo!");  
-     //   sendInformationViaBluetooth (newMovement);
-        digitalWrite(BLUE_LED, HIGH);
-        previousMovement = newMovement;
-      }
-      else if (newMovement == (int) NOT_VALID_GESTURE)
-      {
-        Serial.println("El movimiento NO es nuevo!");  
-        digitalWrite(BLUE_LED, LOW);
-        //rgbColor(LOW, LOW, LOW);
-      }
-      //Serial.println("---");
-    //}
-    /*else
-    {
-      digitalWrite(BLUE_LED, LOW);
-    }*/ 
+      //int test = hand.getHandPosition();
 
-    // Actualizo previousMillis para que vuelva a ingresar en próximas iteraciones.
-    previousMillis = millis();
+      Serial.println("P");
+      Serial.println((double)analogRead(FLEX_SENSOR_PULGAR));
+      //Serial.println(bigFinger.processInformation((double)analogRead(FLEX_SENSOR_PULGAR)));
+      Serial.println(hand.getBigFinger().getActualValue());
+      Serial.println(bigFinger.getActualValue());
+      /*Serial.println(hand.getAux());
+      Serial.println(bigFinger.getActualValue());
+      Serial.println(bigFinger.getAngle());*/
+      
+      Serial.println("\n");
+          
+/*
+      Serial.println("I");
+      Serial.println((double)analogRead(FLEX_SENSOR_INDICE));
+      Serial.println(indexFinger.getActualValue());
+      Serial.println(indexFinger.getAngle());
+      Serial.println("\n");
+
+      Serial.println("M");
+      Serial.println((double)analogRead(FLEX_SENSOR_MEDIO));
+      Serial.println(middleFinger.getActualValue());
+      Serial.println(middleFinger.getAngle());
+      Serial.println("\n");*/
+      /*
+      Serial.println("EVALUO");
+      if (test == 0){
+        Serial.println("STAR");
+      } else if (test == 1) {
+        Serial.println("SCISSORS");
+      } else if (test == 2) {
+        Serial.println("GOOD");
+      } else if (test == 3) {
+        Serial.println("ROCK");
+      }*/
+      
+      
+      // llamar a gesture --> si tuviera unooooooo!!!
+  
+      // Actualizo previousMillis para que vuelva a ingresar en próximas iteraciones.
+    
+      previousMillis = millis();
   }
 
   /*
@@ -185,125 +164,6 @@ void loop()
   */
 }
 
-/*
-   Función utilizada para convertir la medición recibida en ohms de los sensores flex en el ángulo de inclinación correspondiente.
-
-   Recibe:
-   @sensorName: sensor flex que recibe.
-   @straightResistance: medida de la resistencia con el flex completamente abierto.
-   @bedResistance: medida de la resistencia con el flex completamente cerrado.
-   @divisorResistance: medida de la resistencia fija del divisor de tensión.
-
-   Retorna:
-   @angleValue: valor del ángulo en el que se encuentra el flex.
-*/
-double readFlex (const int sensorName, const int straightResistance, const int bendResistance, const int divisorResistance)
-{
-  
-  double sensorValue = (double) analogRead(sensorName); //read new value from sensor in analog pin A1, for example
-
-  //double flexVoltaje = sensorValue * VCC / 1023.0;
-  //double flexResistance = divisorResistance * (VCC / flexVoltaje - 1.0);
-
-  // Uso la resistencia calculada para estimar el angulo de inclinación del sensor
-  double angleValue = map(sensorValue, straightResistance, bendResistance, 0, 90.0);
-
-  Serial.println("---");
-  Serial.println(sensorName);
-  Serial.println(sensorValue);
-  Serial.println(angleValue);
-  Serial.println("---");
-  return angleValue;
-}
-
-/*
-   Función utilizada para identificar la posición de los sensores flex.
-
-   Recibe:
-   @flexAngle: valor del ángulo del sensor flex.
-   
-   Retorna:
-   @flexPosition: posición en la que se encuentra el sensor.   
-
-*/
-int convertAngleIntoPosition (const float flexAngle)
-{
-  int flexPosition;
-
-  if (flexAngle < 40.0)
-  {
-    flexPosition = (int) STRAIGHT_FLEX;
-  }
-  else if (flexAngle > 84.0)
-  { 
-    flexPosition = (int) BEND_FLEX;
-  }
-  else
-  {
-    flexPosition = (int) MEDIUM_BEND_FLEX;
-  }
-
-  return flexPosition;
-}
-
-/*
- * Función utilizada para convertir la información de los sensores en un único valor 
- * a enviar vía Bluetooth a la aplicación Android.
- * 
- * Recibe:
- * @sensorFlexPulgar: posición de sensor flex del pulgar.
- * @sensorFlexIndice: posición de sensor flex del índice.
- * @sensorFlexMedio: posición de sensor flex del medio.
- * @sensorAcelerometer: sentido en el que se movió el acelerómetro.
- * 
- * Retorna:
- * @movement: movimiento realizado.
- */
-int convertInformationIntoMovement (const int sensorFlexPulgar, const int sensorFlexIndice, const int sensorFlexMedio, const int sensorAcelerometer)
-{
-  int movement;
-  if (sensorFlexPulgar == (int) BEND_FLEX)
-  {
-    if (sensorFlexIndice == (int) BEND_FLEX && sensorFlexMedio == (int) BEND_FLEX)
-    {
-  //    rgbColor(255, 0, 0); // Color rojo.
-      return movement = (int) PLAY_PAUSE;  
-    }
-    else if (sensorFlexIndice == (int) STRAIGHT_FLEX)
-    {
-      if (sensorFlexMedio == (int) STRAIGHT_FLEX)
-      {
-  //      rgbColor(0, 255, 0); // Color verde.
-        return movement = (int) SPEED_X2;
-      }
-      else if (sensorFlexMedio == (int) BEND_FLEX)
-      {
-   //     rgbColor(0, 0, 255); // Color azul.
-        return movement = (int) SPEED_X1;
-      }
-    }
-    else if (sensorFlexIndice == (int) MEDIUM_BEND_FLEX && sensorFlexMedio == (int) BEND_FLEX)
-    {
-    //    rgbColor(255, 0, 255); // Color magenta.
-        return movement = (int) SPEED_X05;
-    } 
-  }
-  else if (sensorFlexPulgar == (int) STRAIGHT_FLEX && sensorFlexIndice == (int) STRAIGHT_FLEX && sensorFlexMedio == (int) STRAIGHT_FLEX)
-  {
-    if (sensorAcelerometer == (int) LEFT)
-    {
-   //     rgbColor(255, 255, 0); // Color amarillo.
-        return movement = (int) NEXT;
-    }
-    else if (sensorAcelerometer == (int) RIGHT)
-    {
-   //     rgbColor(255, 255, 125); // Color frambuesa.
-        return movement = (int) PREVIOUS;
-    }
-  }
-  
-  return movement = (int) NOT_VALID_GESTURE;
-}
 
 /*
  * Función utilizada para enviar información vía Bluetooth
@@ -311,10 +171,11 @@ int convertInformationIntoMovement (const int sensorFlexPulgar, const int sensor
  * Recibe:
  * @movement: movimiento a enviar a la aplicación Android.
  */
-void sendInformationViaBluetooth (const int movement) 
+ /*
+void sendInformationViaBluetooth (const int movement)
 {
-//    bluetooth.write(movement);
-}
+  bluetooth.write(movement);
+}*/
 
 
 /*
@@ -324,9 +185,10 @@ void sendInformationViaBluetooth (const int movement)
  * @blueLightValue: cantidad de color azul.
  * @greenLightValue: cantidad de color verde.
  */
+ /*
 void rgbColor (int redLightValue, int greenLightValue, int blueLightValue)
 {
   analogWrite(RGB_LED_RED, redLightValue);
   analogWrite(RGB_LED_BLUE, blueLightValue);
   analogWrite(RGB_LED_GREEN, greenLightValue);
-}
+}*/
